18778ffc5ef7eeb4501c0f9a4e40d74c998781ca
Martin Shaw <mshaw@venditan.com>
2021-04-29 04:42:28 +0100

    Working on all pages

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 >Some little additions to VC's functionality for developer convenience
  
+### How to install.
+
+1. Run 'yarn build'.
+2. Go to 'chrome:extensions'.
+3. Enable 'Developer Mode' (top right).
+4. Click 'Load unpacked'.
+5. Select the 'dist' folder and click 'Select'.
+6. The extension will work on the relevant VC pages.
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,19 @@ import {v4 as uuidv4} from 'uuid';
 import CodeMirror from "codemirror";
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/idea.css';
+import 'codemirror/mode/php/php.js';
+import 'codemirror/mode/javascript/javascript.js';
 
 interface EditorAddon extends CodeMirror.EditorConfiguration {
     height?: string;
     lineNumbers?: boolean;
     autoRefresh?: boolean;
     showTrailingSpace?: boolean;
+}
+
+interface CodeMirrorLanguageMode {
+    name?: string,
+    json?: boolean,
 }
 
 class VCEnhancementsApp {
@@ -43,22 +50,40 @@ class VCEnhancementsApp {
 
     private setupEditors () {
         const self = this;
-        document.getElementById('content_wrap')
-            .getElementsByClassName('span8')[0]
-            .querySelectorAll('textarea.code_textarea')
-            .forEach(function(element) {
-                const id = uuidv4();
-                element.id = id;
 
-                const editorConfig: CodeMirror.EditorConfiguration & EditorAddon = {
-                    height: '350px',
-                    lineNumbers: true
-                };
-                let editor = CodeMirror.fromTextArea(
-                    document.getElementById(id) as HTMLTextAreaElement,
-                    editorConfig
-                );
-            });
+        let editors: NodeListOf<any>;
+        if (self.pageType == 'LayoutBlockInstance' || self.pageType == 'LayoutTemplate' ||  self.pageType == 'LayoutBlockTemplate') {
+            editors = document.getElementById('content_wrap')
+                .getElementsByClassName('span8')[0]
+                .querySelectorAll('textarea.code_textarea');
+        } else if (self.pageType == 'CMSContentTemplate') {
+            editors = document.getElementById('content_wrap')
+                .querySelector('form#edit_template_form')
+                .querySelectorAll('textarea.code_textarea');
+        }
+
+        editors.forEach(function(element: HTMLElement) {
+            const id = uuidv4();
+            element.id = id;
+
+            const label = (element.previousElementSibling as HTMLDivElement).innerText;
+            let lang: CodeMirrorLanguageMode = {name: 'php'};
+            switch (label) {
+                case 'Config (JSON)': lang = {name: 'javascript', json: true}; break;
+                // ...
+            }
+
+            const editorConfig: CodeMirror.EditorConfiguration & EditorAddon = {
+                height: '350px',
+                lineNumbers: true,
+                mode: lang,
+
+            };
+            let editor = CodeMirror.fromTextArea(
+                document.getElementById(id) as HTMLTextAreaElement,
+                editorConfig
+            );
+        });
     }
 
     private setupJumpLinks () {
